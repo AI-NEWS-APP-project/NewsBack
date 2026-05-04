c5423e1624427ba23c6d80c9404d27dc5b71f506
seooooooooook <80299241+seooooooooook@users.noreply.github.com>
2026-05-04 16:59:14 +0900

    fix: fcm bean 버그 수정

---
+++ src/main/java/com/news/newsback/infra/fcm/FcmClientConfig.java
@@ -0,0 +1,23 @@
+정package com.news.newsback.infra.fcm;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FcmClientConfig {
+
+    @Bean
+    @ConditionalOnBean(FirebaseMessaging.class)
+    public FcmClient firebaseFcmClient(FirebaseMessaging firebaseMessaging) {
+        return new FirebaseFcmClient(firebaseMessaging);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(FcmClient.class)
+    public FcmClient noopFcmClient() {
+        return new NoopFcmClient();
+    }
+}
